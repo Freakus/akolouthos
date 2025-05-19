577c313a8b6a974dbeb43dd0a9f31caca450572f
Jay <freakus+github@gmail.com>
2025-05-19 13:46:14 +0100

    No SVG Symbols & Verticality Updates
    - Adjusted Square(ish) button to prefer more rows than columns when not square as a result of discussion with Antiheat regarding proper conventions.
    - Added Save (No SVG Symbols) button which outputs each glyph as it's own path in a nested SVG element instead of using <defs>, <symbols>, and <uses>. This results in a larger file, but hopefully more software will support loading this without issue.

--- src/components/Akolouthos.vue
+++ src/components/Akolouthos.vue
@@ -4,12 +4,13 @@
     </div>
     <div class="center">
     <span>
-        <div><button @click="saveFile" style="width:100%;">Save</button></div>
+        <div><button @click="saveFile(false)" style="width:100%;">Save</button></div>
+        <div><button @click="saveFile(true)" style="width:100%;">Save (No SVG Symbols)</button></div>
         <div><button @click="squareRows" style="width:100%;">Square(ish)</button></div>
-        <div><label for="rows">Rows:</label><input id="rows" type="number" v-model="rows" min="1" max="999999999" size="8"/></div>
-        <div><label for="scale">Width:</label><input id="scale" type="number" v-model="width" min="1" max="999999999" size="8"/></div>
+        <div><label for="rows" style="float:left;">Rows:</label><input id="rows" type="number" v-model="rows" min="1" max="999999999" size="8" style="float:right;" /></div>
+        <div><label for="scale" style="float:left;">Width:</label><input id="scale" type="number" v-model="width" min="1" max="999999999" size="8" style="float:right;" /></div>
     </span>
-    <textarea v-model="text" rows="5" cols="100">{{ text }}</textarea>
+    <textarea v-model="text" rows="7" cols="100">{{ text }}</textarea>
   </div>
   <div :style="SVGStyle" v-html="SVG"></div>
 </template>
@@ -86,8 +87,18 @@ export default {
       }
       return uses;
     },
+    SVGPaths() {
+      var paths = ""
+      for (let index = 0; index < this.validCharacters.length; index++) {
+          paths += `<svg x="${Math.floor(index / this.rows) * 2}" y="${(index % this.rows) * 2}">${this.generatePath(this.validCharacters[index])}</svg>`;
+      }
+      return paths;
+    },
     SVG() {
-        return `<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="100%" viewBox="${this.viewBox}"><defs>${this.SVGSymbols}</defs><g>${this.SVGUses}</g></svg>`
+        return `<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="100%" viewBox="${this.viewBox}">\n<defs>\n${this.SVGSymbols}</defs>\n<g>\n${this.SVGUses}</g>\n</svg>`
+    },
+    SVGNoSymbols() {
+        return `<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="100%" viewBox="${this.viewBox}">\n<g>\n${this.SVGPaths}</g>\n</svg>`
     },
     viewBox() {
       return `0 0 ${ this.cols * 2 } ${ this.rows * 2 }`
@@ -107,22 +118,34 @@ export default {
   // They can be bound as event listeners in templates.
   methods: {
     squareRows() {
-      this.rows = Math.floor(Math.sqrt(this.length));
+      this.rows = Math.ceil(Math.sqrt(this.length));
     },
 
     generateSymbol(character) {
       if (glyphs[character])
-        return `<symbol id="${glyphs[character]["name"]}" width="2" height="2" viewBox="0 0 2 2"><path d="${glyphs[character]["path"]}"/></symbol>`
+        return `<symbol id="${glyphs[character]["name"]}" width="2" height="2" viewBox="0 0 2 2">${this.generatePath(character)}</symbol>\n`
       else
         return ""
     },
+
+    generatePath(character) {
+      if (glyphs[character])
+        return `<path d="${glyphs[character]["path"]}" />\n`
+      else
+        return ""
+    },
+
     mapCharacterToSVGUse(character, index) {
       return `<use href="#${glyphs[character]["name"]}" x="${Math.floor(index / this.rows) * 2}" y="${(index % this.rows) * 2}" style="opacity:1.0" fill="${this.color}" />\n`;
     },
 
-    saveFile() {
+    saveFile(noSymbols=false) {
         const link = document.createElement("a");
-        const blob = new Blob([this.SVG], {type: 'image/svg+xml'});
+        var blob
+        if (noSymbols)
+            blob = new Blob([this.SVGNoSymbols], {type: 'image/svg+xml'});
+        else
+            blob = new Blob([this.SVG], {type: 'image/svg+xml'});
         link.setAttribute('href', URL.createObjectURL(blob));
         link.setAttribute('download', `${this.validCharacters.join("")}.svg`);
         link.click();
